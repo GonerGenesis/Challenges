0400b57bf7e85d4790d09f4e8fe7d8c1947c616f
Gunnar Kistner <gunnar.kistner@gmail.com>
2022-02-17 16:49:25 +0100

    before

--- palindrom.py
+++ palindrom.py
@@ -1,5 +1,6 @@
 import re
 
+
 class CuttingPalindromesPython:
     # def __init__(self, init_string: str):
     #     self.init_string = init_string
@@ -21,36 +22,55 @@ class CuttingPalindromesPython:
             return 0
         else:
             i = 0
-            a, b = self.cut_string(palindrome_string)
-            while len(b) != 0:
-                if not self.is_palindrome(a):
-                    i += self.minimum_palindrome_cuts(a)
-                a, b = self.cut_string(b)
-                i += 1
-            return len(palindrome_string) - 1 if i == 0 else i
+            palindrome_string = [palindrome_string]
+            while len(palindrome_string) != 0:
+                print(palindrome_string)
+                palindroms = []
+                # palindrome_string = palindrome_string.replace(self.cut_string(palindrome_string), "", 1)
+                left = []
+                for string in palindrome_string:
+                    # if len(string) == 1:
+                    #     continue
+                    pal = self.cut_string(string)
+                    string = list(string.partition(pal))
+                    string = list(filter(('').__ne__, string))
+                    string.remove(pal)
+                    left += string
+                    i += 1
+                print(palindrome_string)
+                palindrome_string = left
+                print(palindrome_string)
+            return len(palindrome_string) - 1 if i == 0 else i - 1
 
-    def cut_string(self, string: str) -> (str, str):
+    def cut_string(self, string: str) -> str:
+        # print(string)
         slice_fwd = [m.start() for m in re.finditer(string[0], string)]
         slice_bwd = [m.start() for m in re.finditer(string[-1], string[::-1])]
-        print(string)
-        if len(slice_fwd) == 1:
-            if len(string) == 1:
-                return string, ""
-            else:
-                first = string[0]
-                second = string[1:]
-        else:
-            if len(slice_bwd) > 1 and slice_bwd[1] > slice_fwd[1]:
-                string = string[::-1]
-                slice_fwd = slice_bwd
-            first = string[0]
-            second = string[1:]
-            for d in slice_fwd:
-                if self.is_palindrome(string[:d+1]):
-                    first = string[:d + 1]
-                    second = string[d + 1:]
-        print(first, second)
-        return first, second
+        letters = {}
+        for letter in string:
+            if letter not in letters.keys():
+                pal = letter
+                occ = [m.start() for m in re.finditer(letter, string)]
+                # print("occ", occ)
+                count = len(occ)
+                if count > 1:
+                    for i in range(count - 1):
+                        # print(range(1, count-1))
+                        for j in range(1, count):
+                            is_pal = string[occ[i]:occ[j] + 1]
+                            # print("check", is_pal)
+                            if self.is_palindrome(is_pal) and len(is_pal) > len(pal):
+                                pal = is_pal
+                # print("pal", pal)
+                letters[letter] = pal
+        long_pal = ""
+        # print("letters", letters)
+        for let, pal in letters.items():
+            # print(let, pal)
+            if len(pal) > len(long_pal):
+                long_pal = pal
+        print("longest pal", long_pal)
+        return long_pal
 
 
 if __name__ == '__main__':
@@ -58,11 +78,12 @@ if __name__ == '__main__':
     print(pal.is_palindrome("otto"))
     print(pal.is_palindrome("java"))
     print(pal.is_palindrome("a"))
-    print(pal.minimum_palindrome_cuts("otto"))
-    print(pal.minimum_palindrome_cuts("python"))
-    print(pal.minimum_palindrome_cuts("radarlevel"))
-    print(pal.minimum_palindrome_cuts("radarrefer"))
-    print(pal.minimum_palindrome_cuts("levels"))
+    print("otto", pal.minimum_palindrome_cuts("otto"))
+    print("python", pal.minimum_palindrome_cuts("python"))
+    print("reward", pal.minimum_palindrome_cuts("reward"))
+    print("radarlevel", pal.minimum_palindrome_cuts("radarlevel"))
+    print("radarrefer", pal.minimum_palindrome_cuts("radarrefer"))
+    print("levels", pal.minimum_palindrome_cuts("levels"))
     print("wowaphplevel", pal.minimum_palindrome_cuts("wowaphplevel"))
     print("wowpshplevel", pal.minimum_palindrome_cuts("wowpshplevel"))
     print("wowakayak", pal.minimum_palindrome_cuts("wowakayak"))
